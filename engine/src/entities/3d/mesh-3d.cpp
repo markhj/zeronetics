10c15fa1bc7a405cce9d2a5d6ed3c69087ea24ff
Mark <hello@markhj.net>
2024-07-14 12:00:13 +0200

    Mesh3D controls own global position

--- engine/src/entities/3d/mesh-3d.cpp
+++ engine/src/entities/3d/mesh-3d.cpp
@@ -4,7 +4,6 @@
 std::vector<ZEN::Vertex3D> ZEN::Mesh3D::getVertices() const noexcept {
     auto vertices = m_shape ? m_shape->getVertices() : m_customVertices;
 
-    // @todo: Global space should be controlled by Scene, once implemented
     AABB3D aabb3d = AABB3D::from(vertices);
     Vec3 center = static_cast<Vec3>(aabb3d / 2.0);
     for (auto &vertex: vertices) {

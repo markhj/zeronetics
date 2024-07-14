f9f58557e6a425af8566157f5929dc6272e6a83f
mhj <5261387+markhj@users.noreply.github.com>
2024-07-14 07:51:33 +0200

    Renderer request system set up

--- include/zeronetics/core/enums.h
+++ include/zeronetics/core/enums.h
@@ -135,4 +135,14 @@ namespace ZEN {
 
         TextureUV,
     };
+
+    /**
+     * Types of requests a ZEN::IRenderManager can make
+     * to a ZEN::IRenderer.
+     */
+    enum class RenderManagerRequest {
+        Allocate,
+        Deallocate,
+        Update,
+    };
 }

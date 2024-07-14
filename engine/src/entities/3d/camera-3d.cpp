e15e4ebfcae47af6d6131529ac5a3e1de2be0bb2
Mark <hello@markhj.net>
2024-07-14 12:10:50 +0200

    Improve camera code

--- engine/src/entities/3d/camera-3d.cpp
+++ engine/src/entities/3d/camera-3d.cpp
@@ -3,11 +3,12 @@
 #include "glm/ext/matrix_transform.hpp"
 
 ZEN::MVP ZEN::Camera3D::getModelViewProjection() const noexcept {
-    float aspectRatio = 800.0f / 600.0f;
-    float fovY = glm::radians(45.0f);
-    float nearClip = 0.1f;
-    float farClip = 100.0f;
-    auto projection = glm::perspective(fovY, aspectRatio, nearClip, farClip);
+    auto projection = glm::perspective(fieldOfView,
+                                       aspectRatio,
+                                       clippingPlane[0],
+                                       clippingPlane[1]);
 
-    return {glm::mat4(1.0f), glm::lookAt(position, target, up), projection};
+    return {glm::mat4(1.0f),
+            glm::lookAt(position, target, up),
+            projection};
 }

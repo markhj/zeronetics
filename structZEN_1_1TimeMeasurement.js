25606ecda5bd6a1180264bd9333ab99f1a8d44b8
markhj <markhj@users.noreply.github.com>
2024-07-19 16:24:48 +0000

    deploy: b644574f6ced4627a97b985743ee3b62f074f1bf

--- structZEN_1_1TimeMeasurement.js
+++ structZEN_1_1TimeMeasurement.js
@@ -1,4 +1,5 @@
 var structZEN_1_1TimeMeasurement =
 [
+    [ "toSeconds", "structZEN_1_1TimeMeasurement.html#a316b4cad4a087b727d497da6166d5500", null ],
     [ "microsecs", "structZEN_1_1TimeMeasurement.html#ac07dde346c2d8ca123d367943aa4bf30", null ]
 ];

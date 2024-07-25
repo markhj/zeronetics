6b56d93244a4c80844b1d5736d59db1575014186
mhj <5261387+markhj@users.noreply.github.com>
2024-07-25 15:46:47 +0200

    Improved code for string helpers

--- engine/src/helpers/strings.cpp
+++ engine/src/helpers/strings.cpp
@@ -1,15 +1,28 @@
 #include "zeronetics/helpers/strings.h"
+#include <functional>
+
+namespace {
+    inline std::string characterIter(const std::string &input,
+                                     const std::function<unsigned char(unsigned char)> &algo) noexcept {
+        std::string result = input;
+        std::transform(result.begin(),
+                       result.end(),
+                       result.begin(),
+                       [&](unsigned char c) -> unsigned char {
+                           return algo(c);
+                       });
+        return result;
+    }
+}
 
 std::string ZEN::Strings::toLowerCase(const std::string &input) noexcept {
-    std::string result = input;
-    std::transform(result.begin(), result.end(), result.begin(),
-                   [](unsigned char c) { return std::tolower(c); });
-    return result;
+    return characterIter(input, [](unsigned char c) -> unsigned char {
+        return std::tolower(c);
+    });
 }
 
 std::string ZEN::Strings::toUpperCase(const std::string &input) noexcept {
-    std::string result = input;
-    std::transform(result.begin(), result.end(), result.begin(),
-                   [](unsigned char c) { return std::toupper(c); });
-    return result;
+    return characterIter(input, [](unsigned char c) -> unsigned char {
+        return std::toupper(c);
+    });
 }

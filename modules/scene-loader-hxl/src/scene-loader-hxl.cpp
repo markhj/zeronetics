23160596bf4baa8846ab1322d3e6ed5a880f1eb7
mhj <5261387+markhj@users.noreply.github.com>
2024-08-06 17:19:13 +0200

    HXL support for cubes

--- modules/scene-loader-hxl/src/scene-loader-hxl.cpp
+++ modules/scene-loader-hxl/src/scene-loader-hxl.cpp
@@ -7,15 +7,17 @@
 #include "hxl-lang/hxl-lang.h"
 #include "zeronetics/logging/logging.h"
 
-HXL::Schema schema;
-
-HXL::DeserializationProtocol deserializationProtocol;
-
-std::shared_ptr<ZEN::Camera3D> camera3d;
-
-std::vector<std::shared_ptr<ZEN::IRenderable3D>> list_mesh3d;
-
 namespace {
+    HXL::Schema schema;
+
+    HXL::DeserializationProtocol deserializationProtocol;
+
+    std::shared_ptr<ZEN::Camera3D> camera3d;
+
+    std::vector<std::shared_ptr<ZEN::IRenderable3D>> listMesh3d;
+
+    std::unordered_map<std::string, std::shared_ptr<ZEN::Cube>> listCubes;
+
     void setVec3(ZEN::Vec3 *vec,
                  const char *key,
                  const std::map<std::string, HXL::DeserializedNodeProperty> &data) {
@@ -37,27 +39,50 @@ namespace {
 ZEN::SceneLoaderHxl::SceneLoaderHxl() {
     schema.types.push_back({"Mesh3D"});
     schema.types[0].properties.push_back({"position", HXL::DataType::Float, HXL::ValueStructure::Array});
+    schema.types[0].properties.push_back({"shape", HXL::DataType::NodeRef});
+
+    schema.types.push_back({"Cube"});
+    schema.types[0].properties.push_back({"size", HXL::DataType::Float, HXL::ValueStructure::Array});
 
     schema.types.push_back({"Camera3D"});
     schema.types[0].properties.push_back({"position", HXL::DataType::Float, HXL::ValueStructure::Array});
     schema.types[0].properties.push_back({"target", HXL::DataType::Float, HXL::ValueStructure::Array});
 
-    HXL::DeserializationHandle mesh{"Mesh3D"};
-    mesh.handle = [&](const HXL::DeserializedNode &node) {
-        std::shared_ptr<Mesh3D> mesh = std::make_shared<Mesh3D>(std::make_shared<Cube>(Cube(0.5)));
-        setVec3(&mesh->position, "position", node.properties);
-        list_mesh3d.emplace_back(mesh);
+    HXL::DeserializationHandle dsCube{"Cube"};
+    dsCube.handle = [&](const HXL::DeserializedNode &node) {
+        auto it = node.properties.find("size");
+        auto size = std::get<std::vector<ZEN::gw_float>>((*it).second.value);
+        std::shared_ptr<Cube> cube = std::make_shared<Cube>(Cube(size[0], size[1], size[2]));
+        listCubes[node.name] = cube;
     };
 
-    HXL::DeserializationHandle ds_camera3d{"Camera3D"};
-    ds_camera3d.handle = [&](const HXL::DeserializedNode &node) {
+    HXL::DeserializationHandle dsMesh{"Mesh3D"};
+    dsMesh.handle = [&](const HXL::DeserializedNode &node) {
+        Cube makeCube(0.5);
+        auto it = node.properties.find("shape");
+        if (it != node.properties.end()) {
+            std::string ref = std::get<HXL::NodeRef>((*it).second.value).references;
+            auto it2 = listCubes.find(ref);
+            if (it2 != listCubes.end()) {
+                makeCube = *it2->second;
+            }
+        }
+
+        std::shared_ptr<Mesh3D> mesh = std::make_shared<Mesh3D>(std::make_shared<Cube>(makeCube));
+        setVec3(&mesh->position, "position", node.properties);
+        listMesh3d.emplace_back(mesh);
+    };
+
+    HXL::DeserializationHandle dsCamera3d{"Camera3D"};
+    dsCamera3d.handle = [&](const HXL::DeserializedNode &node) {
         camera3d = std::make_shared<Camera3D>(Camera3D());
         setVec3(&camera3d->position, "position", node.properties);
         setVec3(&camera3d->target, "target", node.properties);
     };
 
-    deserializationProtocol.handles.push_back(mesh);
-    deserializationProtocol.handles.push_back(ds_camera3d);
+    deserializationProtocol.handles.push_back(dsMesh);
+    deserializationProtocol.handles.push_back(dsCamera3d);
+    deserializationProtocol.handles.push_back(dsCube);
 }
 
 std::shared_ptr<ZEN::IScene> ZEN::SceneLoaderHxl::load(const ZEN::File &file) {
@@ -69,7 +94,7 @@ std::shared_ptr<ZEN::IScene> ZEN::SceneLoaderHxl::load(const ZEN::File &file) {
     std::shared_ptr<Scene> scene = std::make_shared<Scene>(Scene());
     HXL::ProcessResult result = HXL::Processor::process(source.result(), schema, deserializationProtocol);
 
-    for (const auto &mesh3d: list_mesh3d) {
+    for (const auto &mesh3d: listMesh3d) {
         scene->add(mesh3d);
     }
 

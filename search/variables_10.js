a5a462373f28bf036309c9b1eedbba8ec5569169
markhj <markhj@users.noreply.github.com>
2024-08-28 15:49:10 +0000

    deploy: b0518f7d3d6cc6f1fdc119707750eb19fce9a6cd

--- search/variables_10.js
+++ search/variables_10.js
@@ -4,7 +4,7 @@ var searchData=
   ['settings_726',['settings',['../classZEN_1_1IRenderLayer.html#a087ded5f954d6b9e4ea2d4d3d48ff696',1,'ZEN::IRenderLayer']]],
   ['shader_727',['shader',['../structZEN_1_1IRenderGroup3D.html#af8e255f798bbd701ff562e2de74b73d1',1,'ZEN::IRenderGroup3D']]],
   ['signals_728',['signals',['../classZEN_1_1ControlSystems_1_1FreeCamera.html#a392188dbfa4024119f6b34ee681f3509',1,'ZEN::ControlSystems::FreeCamera']]],
-  ['size_729',['size',['../structZEN_1_1GPUAllocation.html#ab1eab55ae852a9138e999eb8b46efbae',1,'ZEN::GPUAllocation']]],
+  ['size_729',['size',['../structZEN_1_1Viewport.html#a527eec2e9f6ed10f793d950b3c5671a0',1,'ZEN::Viewport::size()'],['../structZEN_1_1GPUAllocation.html#ab1eab55ae852a9138e999eb8b46efbae',1,'ZEN::GPUAllocation::size()']]],
   ['slotsdirectionallight3d_730',['slotsDirectionalLight3D',['../structZEN_1_1LightSupport.html#a0160a864ae5da3d6f404bf96cc314819',1,'ZEN::LightSupport']]],
   ['slotspointlight3d_731',['slotsPointLight3D',['../structZEN_1_1LightSupport.html#a360e149c1324796b252cda47f6688f61',1,'ZEN::LightSupport']]],
   ['spacing_732',['spacing',['../classZEN_1_1Gizmos_1_1Grid3D.html#adab82f3edccd19503badd218fc105d50',1,'ZEN::Gizmos::Grid3D']]]
